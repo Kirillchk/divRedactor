50cce61f752194f07a2af4bb1a0e1c77083ebb23
capitanDANONE <128199326+capitanDANONE@users.noreply.github.com>
2025-02-19 12:53:21 +0300

    init2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
+        <p class='bg-red w-10 ho'>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
