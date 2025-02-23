4c1e98996c709af688f421b107a3d2b8b01c9240
capitanDANONE <128199326+capitanDANONE@users.noreply.github.com>
2025-02-24 02:02:31 +0300

    2hints

--- src/pages/redactor/redactor.jsx
+++ src/pages/redactor/redactor.jsx
@@ -7,12 +7,28 @@ import { createSignal, Show } from "solid-js";
 function Redactor() {
 	const [getUIshown, setUIshown] = createSignal(false)
 	const [getShowGreen, setShowGreen] = createSignal(false)
-	let body
+	const hint1 = (
+		<div style="display:flex; position: relative;  justify-content: center; align-items: center; ">
+			<div style="z-index: 5; position: absolute; height: 20px; width: 20px; background-color: green;">
+			</div>
+		</div>
+	)
+	const hint2 = (
+		<div style="display:flex; position: relative;  justify-content: center; align-items: center; ">
+			<div style="z-index: 5; position: absolute; height: 20px; width: 20px; background-color: green;">
+			</div>
+		</div>
+	)
 	function handleInsertInto(into, what) {
 		into.innerHTML = what
 	}
-	function fuf(){
-		console.log('asda')
+	function fuf(arg){
+		arg.target.id = 'Is selected element'
+		arg.target.insertBefore(hint1, arg.target.childNode)
+		arg.target.parentNode.insertBefore(hint2, arg.target)
+	}
+	function fuf2(arg){
+		arg.target.id = ''
 	}
 	document.addEventListener('keydown', function(event) {
 		if (event.code === 'KeyH' && event.shiftKey) {
@@ -21,22 +37,18 @@ function Redactor() {
 		}
 	});
   return (
-	<>
-		<div ref={body}>
-			{/*<div style="display:flex; position: relative;  justify-content: center; align-items: center; ">
-				<div style="z-index: 5; position: absolute; height: 20px; width: 20px; background-color: green;">
-				</div>
-			</div>*/}
-			<div draggable='true' class="select-none cursor-move" on:dragstart={fuf}>
-				<div>asdas</div>
+	<div>
+		<div>
+			<div on:mouseenter={fuf} on:mouseleave={fuf2}>
+				asdas
 			</div>
-			<div>
+			<div on:mouseenter={fuf} on:mouseleave={fuf2}>
+				aelement
+			</div >
+			<div on:mouseenter={fuf} on:mouseleave={fuf2}>
 				aelement
 			</div>
-			<div>
-				aelement
-			</div>
-			<div>
+			<div on:mouseenter={fuf} on:mouseleave={fuf2}>
 				aelement
 			</div>
 			<div>
@@ -46,17 +58,17 @@ function Redactor() {
 				aelement
 			</div>
 		</div>
-		<div class="fixed inset-0">
-			<Show when={getUIshown()}>
-				<Header/>
-				<Templates emitDragg={() => console.log('asd')}/>
-				<nav class="fixed inset-y-0 right-0 bg-gray-800 bg-opacity-95 h-screen w-48">
-					<Hierarchy/>
-					<Properties/>
-				</nav>
-			</Show>
-		</div>
-	</>
+		<Show when={getUIshown()}>
+			<div class="fixed inset-0">
+					<Header/>
+					<Templates emitDragg={() => console.log('asd')}/>
+					<nav class="fixed inset-y-0 right-0 bg-gray-800 bg-opacity-95 h-screen w-48">
+						<Hierarchy/>
+						<Properties/>
+					</nav>
+			</div>
+		</Show>
+	</div>
   );
 }
 
