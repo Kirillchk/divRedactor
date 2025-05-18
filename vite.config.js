731910d4b3d464146fb833498d134d571bfa8b2e
capitanDANONE <128199326+Kirillchk@users.noreply.github.com>
2025-05-18 04:13:37 +0300

    da

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [
-		solidPlugin(),
-		tailwindcss()
-	],
-	server: {
-		port: 3000,
-	},
-	build: {
-		target: 'esnext',
-	},
+  plugins: [
+    solidPlugin(),
+    tailwindcss({
+      config: './tailwind.config.js' // Explicit path to config
+    })
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
 });
